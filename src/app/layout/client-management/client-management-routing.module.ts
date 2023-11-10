9e52b45de9c6f9e042c8acd1aefe837da13159e5
icaruuuuu <ericamaelazartecape@gmail.com>
2023-11-10 21:07:00 +0800

    update

--- src/app/layout/client-management/client-management-routing.module.ts
+++ src/app/layout/client-management/client-management-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClientManagementComponent } from './client-management.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: "",
+    component: ClientManagementComponent
+  }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class ClientManagementRoutingModule { }
--- src/app/layout/client-management/client-management.module.ts
+++ src/app/layout/client-management/client-management.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ComponentModule } from '../component/component.module';
+import { ClientManagementComponent } from './client-management.component';
+import { ClientManagementRoutingModule } from './client-management-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ClientManagementComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentModule,
+    ClientManagementRoutingModule,
   ]
 })
 export class ClientManagementModule { }
