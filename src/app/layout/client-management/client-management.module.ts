5c0feb805c745f98ae6e5c3144c60107607ab13e
angelavcnc <144221209+angelavcnc@users.noreply.github.com>
2023-11-10 19:28:59 +0800

    new changes

--- src/app/layout/client-management/client-management.module.ts
+++ src/app/layout/client-management/client-management.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ClientManagementComponent } from './client-management.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ClientManagementModule
   ]
 })
 export class ClientManagementModule { }
