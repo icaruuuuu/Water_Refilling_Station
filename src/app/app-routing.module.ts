03174e7a3d6f0d2d6ea74d151866ce94c3ac1c5c
icaruuuuu <ericamaelazartecape@gmail.com>
2023-12-16 11:37:58 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
+  {path: "",
+  component: AppComponent,
+  children:[
+    {
+      path: "",
+      redirectTo: "login", pathMatch:"prefix",
+    },
   {
     path: "login",
     loadChildren: ()=>import('./login/login.module').then(mod => mod.LoginModule),
@@ -11,6 +19,7 @@ const routes: Routes = [
     path: "",
     loadChildren: ()=>import('./layout/layout.module').then(mod => mod.LayoutModule),
   }
+]}
 ];
 
 @NgModule({
